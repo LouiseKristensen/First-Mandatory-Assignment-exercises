ba53cde0ee48f20ed058e6c1a6f4346e88d93eed
LouiseKristensen <louiseetkristensen@gmail.com>
2025-10-27 18:11:15 +0100

    TWL menu

--- Tristan_Wede_Lind/js/script.js
+++ Tristan_Wede_Lind/js/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
+// Scroll to the top arrow 
 const arrowBtns = document.querySelectorAll(".arrow");
 
-arrowBtns.forEach((btn) => {
+arrowBtns.forEach(btn => {
   btn.addEventListener("click", (e) => {
     e.preventDefault();
     document.body.scrollTop = 0;
@@ -10,20 +11,38 @@ arrowBtns.forEach((btn) => {
   });
 });
 
+// Scroll down menu (remove hidden class from style.css)
+// document.querySelectorAll('nav > ul > li > a').forEach(menuOption => {
+//     menuOption.addEventListener('click', function() {
+        
+//         document.querySelectorAll(`nav > ul > li > a`).forEach(option => {
+//             option.classList.remove('active');
+//         });
+//         this.classList.add('active');
+        
+//        const dataTarget = this.getAttribute('data-target');
+//         const targetElement = document.getElementById(dataTarget);
+//         if(targetElement) {
+//             targetElement.scrollIntoView({ behavior: 'smooth' });
+//         }
+//     });
+// });
 
+
+// Only displaying the clicked menu 
 document.querySelectorAll('nav > ul > li > a').forEach(menuOption => {
-    menuOption.addEventListener('click', function() {
-        
-        // Only the clicked menu option is displayed as selected.
-        document.querySelectorAll(`nav > ul > li > a`).forEach(option => {
-            option.classList.remove('active');
-        });
-        this.classList.add('active');
-        
-       const dataTarget = this.getAttribute('data-target');
-        const targetElement = document.getElementById(dataTarget);
-        if(targetElement) {
-            targetElement.scrollIntoView({ behavior: 'smooth' });
-        }
-    });
+  menuOption.addEventListener('click', function() {
+
+    document.querySelectorAll('nav > ul > li > a').forEach(option => {
+      option.classList.remove('active'); 
+    }); 
+    this.classList.add('active');
+
+    const dataTarget = this.getAttribute('data-target'); 
+    document.querySelector(`#${dataTarget}`).classList.remove('hidden'); 
+    document.querySelectorAll(`.page:not(#${dataTarget})`).forEach(page => {
+      page.classList.add('hidden'); 
+    }); 
+  }); 
 });
+ 
